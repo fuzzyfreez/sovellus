d2f26679487166f0e62144b13856f179f5b49ee7
Your Name <you@example.com>
2023-12-11 01:17:52 +0200

    Create a basic template for login & register

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,27 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+from dotenv import load_dotenv
+import os
 
 db = SQLAlchemy()
+login_manager = LoginManager()
+load_dotenv()
 
 def create_app():
     app = Flask(__name__)
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://yourusername:yourpassword@localhost/yourdbname'
+    app.config['SECRET_KEY'] = os.getenv("SECRET_KEY")
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://fuzzyfreez:xinu@localhost/tsoha'
 
     db.init_app(app)
+    login_manager.init_app(app)
+
+    with app.app_context():
+        from .routes import register_routes
+        register_routes(app)
 
     return app
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
